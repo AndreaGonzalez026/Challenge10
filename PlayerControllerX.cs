de0077924625426a40970003f87fb32f0f9d1330
Andrea Alheli Gonzalez <11069744@uvu.edu>
2024-07-12 21:22:20 -0600

    Add files via upload

--- PlayerControllerX.cs
+++ PlayerControllerX.cs
@@ -1,66 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    private Rigidbody playerRb;
-    private float speed = 2;
-    private float turboSpeedMultiplier = 7f;
-    private GameObject focalPoint;
-    public ParticleSystem smoke;
-    
-    public bool turboAvailable;
-    public bool hasPowerup;
-    public GameObject powerupIndicator;
-    public int powerUpDuration = 5;
-    public float turboDuration = 2.0f;
-    public int turboWait = 5;
+    private const int Speed = 500;
+    private const int TurboSpeed = 1000;
 
-    private float normalStrangth = 10 // how hard to hit enemy without powerup
+    private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
+
+    private Rigidbody _playerRb;
+    private GameObject _focalPoint;
+
+    public bool hasPowerup;
+    public int powerUpDuration = 5;
+    public GameObject powerupIndicator;
+    public ParticleSystem smokeParticle;
 
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
-        focalPoint = GameObject.Find("Focal Point");
-        turboAvailable = true;
+        _playerRb = GetComponent<Rigidbody>();
+        _focalPoint = GameObject.Find("Focal Point");
     }
 
     void Update()
     {
-    Moveforward();
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        int speed = Input.GetKey(KeyCode.Space) ? TurboSpeed : Speed;
 
-        //Set powerup indicator position to beneath player
-        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
-    
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.smokeParticle.Play();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            this.smokeParticle.Stop();
+        }
+
+        _playerRb.AddForce(_focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+
+        // Set powerup indicator position to beneath player
+        this.powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        this.smokeParticle.transform.position = transform.position;
     }
 
-    private void Moveforward()
+    // If Player collides with powerup, activate powerup
+    public void OnTriggerEnter(Collider other)
     {
-       //Add force to player in direction of the focal point (and camera)
-       float verticalInput = Input.GetAxis("Vertical");
-       playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
-
-       if(Input.GetKeyDown(KeyCode.Space) && turboAvailable)
-       {
-          playerRb.AddForce(focalPoint.tranform.forward * speed * verticalInput * turboSpeeedMultiplier, ForceMode.Impulse);
-          TurboCooldown();
-          smoke.Play();
-
-       }
-       else
-       {
-          playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput);
-       }
+        if (other.CompareTag("Powerup"))
+        {
+            this.hasPowerup = true;
+            Destroy(other.gameObject);
+            this.powerupIndicator.gameObject.SetActive(true);
+           StartCoroutine(PowerupCooldown());
+        }
     }
 
-     IEnumerable TurboCooldown()
-     {
-         yield return new MainForSeconds(turboWait);
-         turboAvailable = true;
-         
-     }
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
 
-     // If Player collides with powerup, active powerup
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position;
+
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+        }
+    }
 }
